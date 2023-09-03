57f339a84bd177d7f6c3e91ff21ccfc714b55b5b
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2023-09-02 19:01:26 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Character Sets
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const specialCharacters = '!@#$%^&*_+';
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
