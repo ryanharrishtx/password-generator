84516162431535ab84c01f80d0b8a87fcf44db03
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2023-09-04 22:17:02 -0500

    working, but no error handling if user chooses not to include any characters

--- script.js
+++ script.js
@@ -11,15 +11,14 @@ let generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", function() {
 // Prompt user to choose a password length between 8 and 128 characters
 let passwordLength = prompt(`Please choose a password length between 8 and 128 characters.`);
-
 // If password length is between 8 and 128 characters, then ask user if they would like to include lowercase letters, uppercase letters, numbers, and/or special characters
 if (passwordLength >= 8 && passwordLength <= 128) {
-    let includeLowerCase = confirm(`Would you like to include lowercase letters?`);
-    let includeUpperCase = confirm(`Would you like to include uppercase letters?`);
-    let includeNumbers = confirm(`Would you like to include numbers?`);
-    let includeSpecialCharacters = confirm(`Would you like to include special characters?`);
+  let includeLowerCase = confirm(`Would you like to include lowercase letters?`);
+  let includeUpperCase = confirm(`Would you like to include uppercase letters?`);
+  let includeNumbers = confirm(`Would you like to include numbers?`);
+  let includeSpecialCharacters = confirm(`Would you like to include special characters?`);
 // set up passwordCharacters variable to be an empty array
-let passwordCharacters = [];
+  let passwordCharacters = [];
 // if user chooses to include any of the below, then add the values inside of their respective arrays to passwordCharacters array
   if (includeLowerCase) {
     passwordCharacters += lowerCase;
@@ -33,22 +32,16 @@ let passwordCharacters = [];
   if (includeSpecialCharacters) {
     passwordCharacters += specialCharacters;
   }
-  
 // set password variable to be an empty string
   let password = ``;
 // for loop that runs for the length of the password, and each time it runs, it adds a random character from the passwordCharacters array to the password variable
   for (let i = 0; i < passwordLength; i++) {
     password = password + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
   }
+// set the text content of the element with ID of password to be the password variable
+  document.getElementById(`password`).textContent = password;
+// if user does not choose a password length between 8 and 128 characters, then alert them to choose a password length between 8 and 128 characters
+} else {
+  alert(`Please choose a password length between 8 and 128 characters.`);
 }
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)});
+});
